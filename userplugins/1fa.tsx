840ee99c790c951c1193639505c3f9d1f21a2f59
VR <101288516+VillainsRule@users.noreply.github.com>
2024-11-07 21:56:50 -0500

    2fa ignorer

---
+++ userplugins/1fa.tsx
@@ -0,0 +1,21 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "1FA",
+    description: "never get bothered about 2fa moderation again!",
+    authors: [
+        {
+            id: 1003477997728313405n,
+            name: "Death",
+        },
+    ],
+    patches: [
+        {
+            find: "APPLICATION_SUBSCRIPTION_EXPIRATION:",
+            replacement: [{
+                match: /GUILD_MFA_WARNING:return/,
+                replace: `GUILD_MFA_WARNING:return;return`
+            }]
+        }
+    ]
+});
