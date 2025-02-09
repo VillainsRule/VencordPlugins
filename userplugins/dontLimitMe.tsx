046f71f05616797a4b46d303cbeb0d75a90201cc
VR <101288516+VillainsRule@users.noreply.github.com>
2025-02-09 11:11:40 -0500

    update desc

--- userplugins/dontLimitMe.tsx
+++ userplugins/dontLimitMe.tsx
@@ -8,7 +8,7 @@ import definePlugin from "@utils/types";
 
 export default definePlugin({
     name: "DontLimitMe",
-    description: "removes the limit on message sending. spam away!",
+    description: "removes the client-side limit on message sending - spam away!",
     authors: [{ name: 'Death', id: 1003477997728313405n }],
     patches: [
         {
