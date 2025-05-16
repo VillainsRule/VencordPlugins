c6dbb6b8b40e6ca34e63a0aa4472efa9c6946e24
VR <101288516+VillainsRule@users.noreply.github.com>
2025-05-15 22:18:57 -0400

    fix linting errors

--- userplugins/dontLimitMe.tsx
+++ userplugins/dontLimitMe.tsx
@@ -1,6 +1,6 @@
 /*
  * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -8,8 +8,8 @@ import definePlugin from "@utils/types";
 
 export default definePlugin({
     name: "DontLimitMe",
-    description: "removes the client-side limit on message sending - spam away!",
-    authors: [{ name: 'Death', id: 1003477997728313405n }],
+    description: "removes the limit on message sending. spam away!",
+    authors: [{ name: "Death", id: 1003477997728313405n }],
     patches: [
         {
             find: "cancelQueueMetricTimers",
