59fb2a90dc4c202de1fe2d4c1953a6c4573c600e
VR <101288516+VillainsRule@users.noreply.github.com>
2025-06-27 21:56:21 -0400

    add Vendicated/Vencord#3362

---
+++ from-pr/3362-hideMessage.tsx
@@ -0,0 +1,44 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+// https://github.com/Vendicated/Vencord/pull/3362
+// adapted: inlined author
+// all credit goes to https://github.com/scattagain
+
+import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
+import { DeleteIcon } from "@components/Icons";
+import definePlugin from "@utils/types";
+import { FluxDispatcher, Menu } from "@webpack/common";
+
+const messageCtxPatch: NavContextMenuPatchCallback = (children, { channel, message }) => {
+    const group = findGroupChildrenByChildId("delete", children) ?? findGroupChildrenByChildId("report", children);
+    if (!group || message.deleted) return;
+
+    group.push((
+        <Menu.MenuItem
+            id="vc-hide"
+            label="Hide Message"
+            icon={DeleteIcon}
+            action={async () => {
+                FluxDispatcher.dispatch({
+                    type: "MESSAGE_DELETE",
+                    channelId: channel.id,
+                    id: message.id,
+                    mlDeleted: true
+                });
+            }}
+        />
+    ));
+};
+
+export default definePlugin({
+    name: "HideMessage",
+    description: "Adds a button to the context menu to locally hide a message (reload to reset)",
+    authors: [{ name: "Scatt", id: 1098234477626544180n, }],
+    contextMenus: {
+        "message": messageCtxPatch
+    },
+});
